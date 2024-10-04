0c41d46120f6ca50e8cb3ef58501590bfdcd2635
firoliyos <firatesfaye44@gmail.com>
2024-10-04 07:01:03 +0300

    functions work and styles added

--- star-rating/src/components/star-rating/index.jsx
+++ star-rating/src/components/star-rating/index.jsx
@@ -1,18 +1,20 @@
 import {FaStar} from 'react-icons/fa'
 import {useState} from 'react'
-export default function StarRating(noOfStars) {
+import './styles.css'
+
+export default function StarRating({noOfStars}) {
 
   const [rating, setRating] = useState(0)
   const [hover, setHover] = useState(0)
 
   function handleClick(getCurrentIndex){
-    console.log(getCurrentIndex)
+    setRating(getCurrentIndex)
   }
   function handleMouseEnter(getCurrentIndex){
-    console.log(getCurrentIndex)
+    setHover(getCurrentIndex)
   }
-  function handleMouseLeave(getCurrentIndex){
-    console.log(getCurrentIndex)
+  function handleMouseLeave(){
+    setHover(rating)
   }
 
     return(
@@ -21,13 +23,16 @@ export default function StarRating(noOfStars) {
         [...Array(noOfStars)].map((_,index)=> {
            index += 1
 
-            return <FaStar
+            return (
+              <FaStar
             key={index}
+            className={index <= (hover || rating) ? 'active' : 'inactive'}
             onClick={()=> handleClick(index)}
             onMouseMove={() => handleMouseEnter(index)}
-            onMouseLeave={ ()=> handleMouseLeave(index)}
+            onMouseLeave={ ()=> handleMouseLeave()}
             size={40}
             />
+            ) 
         })
       }
     </div>
