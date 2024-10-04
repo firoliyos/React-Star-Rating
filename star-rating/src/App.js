c95bc64a9e3315c7fadc90040dc54ceecc589a3b
firoliyos <firatesfaye44@gmail.com>
2024-10-04 06:39:18 +0300

    files arranged in components folder

--- star-rating/src/App.js
+++ star-rating/src/App.js
@@ -1,10 +1,10 @@
 import './App.css'
-import StarRating from './components/index'
+import StarRating from './components/star-rating/index'
  function App() {
   return (
     <div className="App">
       <h1>Star Rating App</h1>
-      <StarRating
+      <StarRating noOfStars={10}
       />
     </div>
   );
